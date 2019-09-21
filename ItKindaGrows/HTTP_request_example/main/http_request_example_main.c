5e993e3d4a96c4e05912dc1b660e2f5c15575579
Alec Fuglem <afuglem@purdue.edu>
2019-09-21 09:19:56 -0500

    Got a code...

--- ItKindaGrows/HTTP_request_example/main/http_request_example_main.c
+++ ItKindaGrows/HTTP_request_example/main/http_request_example_main.c
@@ -1,11 +1,11 @@
 /* HTTP GET Example using plain POSIX sockets
 
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
+ This example code is in the Public Domain (or CC0 licensed, at your option.)
 
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
+ Unless required by applicable law or agreed to in writing, this
+ software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ CONDITIONS OF ANY KIND, either express or implied.
+ */
 #include <string.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
@@ -23,113 +23,133 @@
 #include "lwip/dns.h"
 
 /* Constants that aren't configurable in menuconfig */
-#define WEB_SERVER "http://marijauto.com"
+#define WEB_SERVER "marijauto.com"
 #define WEB_PORT 80
 #define WEB_URL "/node/updateStatusNode/"
 
+//marijauto.com/node/updateStatusNode/
+
 static const char *TAG = "example";
 
-static const char *REQUEST = "POST " WEB_URL " HTTP/1.0\r\n"
-    "Host: "WEB_SERVER"\r\n"
-    "Content-Type: application/x-www-form-urlencoded\r\n"
-    "node_id=12345678123456781234567812345678&user_name=admin&node_status=%7B%22status%22%3A%22good%22%7D\r\n";
+//static const char *REQUEST = "POST " WEB_URL " HTTP/1.0\r\n"
+//"Host: "WEB_SERVER"\r\n"
+//"Content-Type: application/x-www-form-urlencoded\r\n"
+//"node_id=12345678123456781234567812345678&user_name=admin&node_status=%7B%22status%22%3A%22good%22%7D\r\n";
+
+static const char *REQUEST = "POST /node/updateStatusNode/ HTTP/1.1"
+   "Host: marijauto.com"
+   "Accept: */*"
+   "User-Agent: Mozilla/5.0 (compatible; Rigor/1.0.0; http://rigor.com)"
+   "Content-Type: application/x-www-form-urlencoded"
+   "Content-Length: 100"
+   "node_id=12345678123456781234567812345678&user_name=admin&node_status=%7B%22status%22%3A%22good%22%7D\r\n";
 
 static void http_get_task(void *pvParameters)
 {
-    const struct addrinfo hints = {
-        .ai_family = AF_INET,
-        .ai_socktype = SOCK_STREAM,
-    };
-    struct addrinfo *res;
-    struct in_addr *addr;
-    int s, r;
-    char recv_buf[64];
+   const struct addrinfo hints =
+   {
+      .ai_family = AF_INET,
+      .ai_socktype = SOCK_STREAM,
+   };
+   struct addrinfo *res;
+   struct in_addr *addr;
+   int s, r;
+   char recv_buf[64];
 
-    while(1) {
-        int err = getaddrinfo(WEB_SERVER, "80", &hints, &res);
+   while(1)
+   {
+      int err = getaddrinfo(WEB_SERVER, "80", &hints, &res);
 
-        if(err != 0 || res == NULL) {
-            ESP_LOGE(TAG, "DNS lookup failed err=%d res=%p", err, res);
-            vTaskDelay(1000 / portTICK_PERIOD_MS);
-            continue;
-        }
+      if(err != 0 || res == NULL)
+      {
+         ESP_LOGE(TAG, "DNS lookup failed err=%d res=%p", err, res);
+         vTaskDelay(1000 / portTICK_PERIOD_MS);
+         continue;
+      }
 
-        /* Code to print the resolved IP.
+      /* Code to print the resolved IP.
 
-           Note: inet_ntoa is non-reentrant, look at ipaddr_ntoa_r for "real" code */
-        addr = &((struct sockaddr_in *)res->ai_addr)->sin_addr;
-        ESP_LOGI(TAG, "DNS lookup succeeded. IP=%s", inet_ntoa(*addr));
+       Note: inet_ntoa is non-reentrant, look at ipaddr_ntoa_r for "real" code */
+      addr = &((struct sockaddr_in *)res->ai_addr)->sin_addr;
+      ESP_LOGI(TAG, "DNS lookup succeeded. IP=%s", inet_ntoa(*addr));
 
-        s = socket(res->ai_family, res->ai_socktype, 0);
-        if(s < 0) {
-            ESP_LOGE(TAG, "... Failed to allocate socket.");
-            freeaddrinfo(res);
-            vTaskDelay(1000 / portTICK_PERIOD_MS);
-            continue;
-        }
-        ESP_LOGI(TAG, "... allocated socket");
+      s = socket(res->ai_family, res->ai_socktype, 0);
+      if(s < 0)
+      {
+         ESP_LOGE(TAG, "... Failed to allocate socket.");
+         freeaddrinfo(res);
+         vTaskDelay(1000 / portTICK_PERIOD_MS);
+         continue;
+      }
+      ESP_LOGI(TAG, "... allocated socket");
 
-        if(connect(s, res->ai_addr, res->ai_addrlen) != 0) {
-            ESP_LOGE(TAG, "... socket connect failed errno=%d", errno);
-            close(s);
-            freeaddrinfo(res);
-            vTaskDelay(4000 / portTICK_PERIOD_MS);
-            continue;
-        }
+      if(connect(s, res->ai_addr, res->ai_addrlen) != 0)
+      {
+         ESP_LOGE(TAG, "... socket connect failed errno=%d", errno);
+         close(s);
+         freeaddrinfo(res);
+         vTaskDelay(4000 / portTICK_PERIOD_MS);
+         continue;
+      }
 
-        ESP_LOGI(TAG, "... connected");
-        freeaddrinfo(res);
+      ESP_LOGI(TAG, "... connected");
+      freeaddrinfo(res);
 
-        if (write(s, REQUEST, strlen(REQUEST)) < 0) {
-            ESP_LOGE(TAG, "... socket send failed");
-            close(s);
-            vTaskDelay(4000 / portTICK_PERIOD_MS);
-            continue;
-        }
-        ESP_LOGI(TAG, "... socket send success");
+      if(write(s, REQUEST, strlen(REQUEST)) < 0)
+      {
+         ESP_LOGE(TAG, "... socket send failed");
+         close(s);
+         vTaskDelay(4000 / portTICK_PERIOD_MS);
+         continue;
+      }
+      ESP_LOGI(TAG, "... socket send success");
 
-        struct timeval receiving_timeout;
-        receiving_timeout.tv_sec = 5;
-        receiving_timeout.tv_usec = 0;
-        if (setsockopt(s, SOL_SOCKET, SO_RCVTIMEO, &receiving_timeout,
-                sizeof(receiving_timeout)) < 0) {
-            ESP_LOGE(TAG, "... failed to set socket receiving timeout");
-            close(s);
-            vTaskDelay(4000 / portTICK_PERIOD_MS);
-            continue;
-        }
-        ESP_LOGI(TAG, "... set socket receiving timeout success");
+      struct timeval receiving_timeout;
+      receiving_timeout.tv_sec = 5;
+      receiving_timeout.tv_usec = 0;
+      if(setsockopt(s, SOL_SOCKET, SO_RCVTIMEO, &receiving_timeout,
+         sizeof(receiving_timeout)) < 0)
+      {
+         ESP_LOGE(TAG, "... failed to set socket receiving timeout");
+         close(s);
+         vTaskDelay(4000 / portTICK_PERIOD_MS);
+         continue;
+      }
+      ESP_LOGI(TAG, "... set socket receiving timeout success");
 
-        /* Read HTTP response */
-        do {
-            bzero(recv_buf, sizeof(recv_buf));
-            r = read(s, recv_buf, sizeof(recv_buf)-1);
-            for(int i = 0; i < r; i++) {
-                putchar(recv_buf[i]);
-            }
-        } while(r > 0);
+      /* Read HTTP response */
+      do
+      {
+         bzero(recv_buf, sizeof(recv_buf));
+         r = read(s, recv_buf, sizeof(recv_buf) - 1);
+         for(int i = 0; i < r; i++)
+         {
+            putchar(recv_buf[i]);
+         }
+      } while(r > 0);
 
-        ESP_LOGI(TAG, "... done reading from socket. Last read return=%d errno=%d.", r, errno);
-        close(s);
-        for(int countdown = 10; countdown >= 0; countdown--) {
-            ESP_LOGI(TAG, "%d... ", countdown);
-            vTaskDelay(1000 / portTICK_PERIOD_MS);
-        }
-        ESP_LOGI(TAG, "Starting again!");
-    }
+      ESP_LOGI(TAG, "... done reading from socket. Last read return=%d errno=%d.", r, errno);
+      close(s);
+      for(int countdown = 10; countdown >= 0; countdown--)
+      {
+         ESP_LOGI(TAG, "%d... ", countdown);
+         vTaskDelay(1000 / portTICK_PERIOD_MS);
+      }
+      ESP_LOGI(TAG, "Starting again!");
+   }
 }
 
 void app_main()
 {
-    ESP_ERROR_CHECK( nvs_flash_init() );
-    tcpip_adapter_init();
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
+   ESP_ERROR_CHECK(nvs_flash_init());
+   tcpip_adapter_init();
+   ESP_ERROR_CHECK(esp_event_loop_create_default());
 
-    /* This helper function configures Wi-Fi or Ethernet, as selected in menuconfig.
-     * Read "Establishing Wi-Fi or Ethernet Connection" section in
-     * examples/protocols/README.md for more information about this function.
-     */
-    ESP_ERROR_CHECK(example_connect());
+   /* This helper function configures Wi-Fi or Ethernet, as selected in menuconfig.
+    * Read "Establishing Wi-Fi or Ethernet Connection" section in
+    * examples/protocols/README.md for more information about this function.
+    */
+   ESP_ERROR_CHECK(example_connect());
 
-    xTaskCreate(&http_get_task, "http_get_task", 4096, NULL, 5, NULL);
+   xTaskCreate(&http_get_task, "http_get_task", 4096, NULL, 5, NULL);
 }
